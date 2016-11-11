e931c4f7487903952577b93583e8a01081f66c11
Ilan Dubois <idubois@advanced-schema.com>
2016-11-11 20:55:47 +0100

    Changes the message when a message is intercepted

--- src/smtpListener.py
+++ src/smtpListener.py
@@ -1,12 +1,9 @@
-import smtpd, subprocess, os, tempfile
+import smtpd, subprocess, os, tempfile, io
 import sendgrid
 from sendgrid.helpers.mail import *
 
 def sendMail(sender, receiver, subject, message):
-    print(sender)
-    print(receiver)
-    print(subject)
-    print(message)
+    print("Email to <" + receiver + "> from <" + sender + "> intercepted")
     sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
     from_email = Email(sender)
     to_email = Email(receiver)
@@ -16,21 +13,24 @@ def sendMail(sender, receiver, subject, message):
     return response.status_code
 
 def editMail(message, mailfrom, rcpttos):
+    subject = "foo"
+    buf = io.StringIO(message)
+    line = buf.readline()
+    while not line.startswith("Subject: ") and len(line) > 0:
+        line = buf.readline()
+    subject = line[9:]
+
     with tempfile.NamedTemporaryFile(delete = False) as f:
         f.write(str.encode(message))
         f.close()
         cmd = os.environ.get('EDITOR', 'vi') + ' ' + f.name
         subprocess.call(cmd, shell=True)
         with open(f.name) as dataFile:
-            sendMail(mailfrom, rcpttos[0], "test", dataFile.read())
+            sendMail(mailfrom, rcpttos[0], subject, dataFile.read())
             dataFile.close()
 
 class SmtpListener(smtpd.SMTPServer):
 
     def process_message(self, peer, mailfrom, rcpttos, data):
-        print('Receiving message from:', peer)
-        print('Message addressed from:', mailfrom)
-        print('Message addressed to  :', rcpttos)
-        print('Message length        :', len(data))
         editMail(data, mailfrom, rcpttos)
         return
