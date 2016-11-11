4a959a92bc01d4db514733923481d1690236811b
Ilan Dubois <idubois@advanced-schema.com>
2016-11-11 19:54:51 +0100

    Sends the email to the receiver

--- src/smtpListener.py
+++ src/smtpListener.py
@@ -1,15 +1,29 @@
 import smtpd, subprocess, os, tempfile
+import sendgrid
+from sendgrid.helpers.mail import *
 
-def editMail(message):
+def sendMail(sender, receiver, subject, message):
+    print(sender)
+    print(receiver)
+    print(subject)
+    print(message)
+    sg = sendgrid.SendGridAPIClient(apikey=os.environ.get('SENDGRID_API_KEY'))
+    from_email = Email(sender)
+    to_email = Email(receiver)
+    content = Content("text/plain", message)
+    mail = Mail(from_email, subject, to_email, content)
+    response = sg.client.mail.send.post(request_body=mail.get())
+    return response.status_code
+
+def editMail(message, mailfrom, rcpttos):
     with tempfile.NamedTemporaryFile(delete = False) as f:
         f.write(str.encode(message))
         f.close()
         cmd = os.environ.get('EDITOR', 'vi') + ' ' + f.name
         subprocess.call(cmd, shell=True)
         with open(f.name) as dataFile:
-            print(dataFile.read())
+            sendMail(mailfrom, rcpttos[0], "test", dataFile.read())
             dataFile.close()
-
 
 class SmtpListener(smtpd.SMTPServer):
 
@@ -18,5 +32,5 @@ class SmtpListener(smtpd.SMTPServer):
         print('Message addressed from:', mailfrom)
         print('Message addressed to  :', rcpttos)
         print('Message length        :', len(data))
-        editMail(data)
+        editMail(data, mailfrom, rcpttos)
         return
